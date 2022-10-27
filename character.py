8c9fcf7b4e74ecee9bedab4b08ac868d610ddf27
Nelson Lu <nelkalmlu@gmail.com>
2022-10-27 16:45:06 -0700

    assets updates

--- character.py
+++ character.py
@@ -7,15 +7,16 @@ class Character():
 
     """A character class representing a character object."""
 
-    def __init__(self, x, y, animation_list) -> None:
+    def __init__(self, x, y, mob_animations, character_type) -> None:
         self._rectangle = pygame.Rect(0, 0, 50, 50)
         self._rectangle.center = (x, y)
-        self._animation_list = animation_list
+        self._character_type = character_type
+        self._animation_list = mob_animations[character_type]
         self._frame_index = 0
         self._action = 0    # 0 = idle, 1 = running
         self._update_time = pygame.time.get_ticks()
         self._running = False
-        self._image = animation_list[self._action][self._frame_index]
+        self._image = self._animation_list[self._action][self._frame_index]
         self._flip = False
 
     def draw(self, surface):
--- main.py
+++ main.py
@@ -18,21 +18,45 @@ moving_right = False
 moving_up = False
 moving_down = False
 
-# Load player animation
-animation_types = ["IDLE", "WALK"]
-animation_list = []
-for animation in animation_types:
-    # Reset temp list of images
-    temp_list = []
-    for i in range(10):
-        image = pygame.image.load(
-            f"assets/sprites/Heroes/{animation}/Fairy_01__{animation}_00{i}.png").convert_alpha()
-        image = scale_image(image, SCALE)
-        temp_list.append(image)
-    animation_list.append(temp_list)
+# Load character images
+mob_animations = []
+
+# indices: demon: 0, dragon: 1, jinn: 2, lizard: 3, medusa: 4, small_dragon: 5
+# Heroes: 6, boss_wraith: 7
+mob_types = ['demon', 'dragon', 'jinn', 'lizard', 'medusa', 'small_dragon']
+main_types = ['Heroes', 'boss_wraith']
+
+# Load animation
+animation_types = ["Idle", "Walk"]
+
+for mob in mob_types:
+    animation_list = []
+    for animation in animation_types:
+        # Reset temp list of images
+        temp_list = []
+        for i in range(1, 4):
+            image = pygame.image.load(
+                f"assets/sprites/Characters/{mob}/{animation}/{animation}{i}.png").convert_alpha()
+            image = scale_image(image, SCALE)
+            temp_list.append(image)
+        animation_list.append(temp_list)
+    mob_animations.append(animation_list)
+
+for mob in main_types:
+    animation_list = []
+    for animation in animation_types:
+        # Reset temp list of images
+        temp_list = []
+        for i in range(1, 11):
+            image = pygame.image.load(
+                f"assets/sprites/Characters/{mob}/{animation}/{animation}{i}.png").convert_alpha()
+            image = scale_image(image, SCALE)
+            temp_list.append(image)
+        animation_list.append(temp_list)
+    mob_animations.append(animation_list)
 
 # Create player
-player = Character(100, 100, animation_list)
+player = Character(100, 100, mob_animations, 6)
 
 # Create the game loop
 run = True
